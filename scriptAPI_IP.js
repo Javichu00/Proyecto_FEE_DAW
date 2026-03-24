f92757a0fe6c89def3cafcefbd4332128c3f1242
Javi <fercho7649@gmail.com>
2026-03-24 11:15:54 +0100

    feat: update Dockerfile and add nginx configuration for API proxy

--- scriptAPI_IP.js
+++ scriptAPI_IP.js
@@ -1,2 +1,3 @@
-const API = 'https://backendfee-production.up.railway.app';
+// const API = 'https://backendfee-production.up.railway.app';
 // const API = 'http://192.168.128.162:8084';
+const API = '/api';
