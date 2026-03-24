cf50aad4c97e0ae5febe28ba66545ad5da9f146e
Javi <fercho7649@gmail.com>
2026-03-24 13:23:54 +0100

    feat: add difficulty label to provider form and update API endpoint

--- scriptAPI_IP.js
+++ scriptAPI_IP.js
@@ -1,3 +1,3 @@
-// const API = 'https://backendfee-production.up.railway.app';
+const API = 'https://backend-production-1315.up.railway.app';
 // const API = 'http://192.168.128.162:8084';
-const API = '/api';
+// const API = '/api';
